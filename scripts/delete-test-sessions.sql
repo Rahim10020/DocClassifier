c4b17e21c3e876665a975489995998e112dbcb6d
Rahim10020 <rahialighi@gmail.com>
2025-10-31 13:09:06 +0000

    essai de resolution du souci d'extraction

--- scripts/delete-test-sessions.sql
+++
@@ -1,8 +0,0 @@
--- Script pour supprimer les sessions de test avec des chemins invalides
--- Exécutez ce script avec: npx prisma db execute --file ./scripts/delete-test-sessions.sql --schema ./prisma/schema.prisma
-
--- Supprimer les documents qui ont des chemins de fichiers invalides (contenant des slashes)
-DELETE FROM "Document" WHERE "fileName" LIKE '%/%' OR "fileName" LIKE '%\%';
-
--- Supprimer les sessions qui n'ont plus de documents
-DELETE FROM "Session" WHERE id NOT IN (SELECT DISTINCT "sessionId" FROM "Document");
